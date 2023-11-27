646bf52c6fe1f521bf885b88fa9505baaf7116e1
dnt201 <nhatd2@vng.com.vn>
2023-11-27 23:17:59 +0700

    vl

--- src/pages/playReactFlow/SettingsModal/index.tsx
+++ src/pages/playReactFlow/SettingsModal/index.tsx
@@ -1,10 +1,14 @@
-import { Modal, ModalProps, Select } from 'antd';
+import { Button, Modal, ModalProps, Select, Space } from 'antd';
 import React, { useEffect, useState } from 'react';
 import './style.less';
 import { useAtom } from 'jotai';
 import { languagesAtom, listLanguageSystem } from '..';
 import { tLanguage } from '../CustomNode';
 import { iOption } from '@interfaces/index';
+import { PlusOutlined } from '@ant-design/icons';
+
+const filterOption = (input: string, option?: { label: string; value: string }) =>
+   (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
 
 interface SettingsModalProps extends ModalProps {
    setShowModal: (b: boolean) => void;
@@ -16,10 +20,10 @@ type ListMenu = {
 }[];
 const listMenu: ListMenu = [{ label: 'Language settings', value: 'language' }];
 const SettingsModal: React.FC<SettingsModalProps> = (props) => {
-   const { setShowModal, ...propsModal } = props;
    const [curSettings, setCurSettings] = useState<SettingState>('language');
-   const [languages, setLanguages] = useAtom(languagesAtom);
+   const [curLanguage, setCurLanguage] = useState<tLanguage>();
 
+   const [languages, setLanguages] = useAtom(languagesAtom);
    const [listExistLanguage, setListExistLanguage] = useState<iOption[]>([]);
    useEffect(() => {
       let tempOption: { value: string; label: string }[] = [];
@@ -52,8 +56,23 @@ const SettingsModal: React.FC<SettingsModalProps> = (props) => {
             <div className="content">
                {curSettings === 'language' ? (
                   <>
-                     <span>Select language</span>
-                     <Select options={listExistLanguage} />
+                     <div className="select-language">
+                        <span>Select language</span>
+                        <Space direction="horizontal" className="space">
+                           <Select
+                              style={{ flex: 1 }}
+                              options={listExistLanguage}
+                              placeholder="Select a language"
+                              filterOption={filterOption}
+                           />
+                           <Button type="dashed">
+                              <i>
+                                 <PlusOutlined />
+                              </i>
+                              Add
+                           </Button>
+                        </Space>
+                     </div>
                   </>
                ) : null}
             </div>
